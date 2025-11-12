62cec7ad1d67bd001736b82847cf189fba3398b0
SATHISH BSK <sathish.bsk786@gmail.com>
2025-11-12 23:07:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig(esbuild: {
-  loader: 'jsx',
-  include: /src\/.*\.js$/,
-},
-)} from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  build: {
+    // Optional custom esbuild settings
+    target: 'esnext',
+  },
+})
